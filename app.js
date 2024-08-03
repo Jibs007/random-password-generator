30e080c0538910b2722560ceb71a93bab61ab7a3
Jibola Obiwole <jizzy10718@gmail.com>
2024-08-03 01:33:17 +0100

    added logic

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-    "/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?", "/"];
+
+let passwordFieldOne = document.getElementById("password-field-one")
+let passwordFieldTwo = document.getElementById("password-field-two")
+
+function generatePassword() {
+    let password1 = ""
+    let password2 = ""
+
+    for (let i = 0; i < 15; i++) {
+        let randomIndex1 = Math.floor(Math.random() * characters.length)
+        let randomIndex2 = Math.floor(Math.random() * characters.length)
+
+        password1 += characters[randomIndex1]
+        password2 += characters[randomIndex2]
+    }
+
+    passwordFieldOne.value = password1
+    passwordFieldTwo.value = password2
+
+}
